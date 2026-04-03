97f71d8e1df6eb6b928e8db06d4b74cfa95698e7
jihye525 <104695986+jihye525@users.noreply.github.com>
2026-04-03 21:52:35 +0900

    Update HealthCheckController.java

--- src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
+++ src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
