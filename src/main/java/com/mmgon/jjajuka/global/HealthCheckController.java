5264f2daec00026221c595823d3114f419c7b402
jihye <jihye07201@gmail.com>
2026-04-03 21:47:05 +0900

    requestmapping 의존성

--- src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
+++ src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckController {
 
     @GetMapping("/health")
