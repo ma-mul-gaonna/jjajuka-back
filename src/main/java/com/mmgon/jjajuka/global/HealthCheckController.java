ff55c6c02b4a7ebf47630a216c776eddffdcb02d
jihye525 <104695986+jihye525@users.noreply.github.com>
2026-04-03 20:47:40 +0900

    Update HealthCheckController.java
    Requestmapping 수정

--- src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
+++ src/main/java/com/mmgon/jjajuka/global/HealthCheckController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckController {
 
     @GetMapping("/health")
