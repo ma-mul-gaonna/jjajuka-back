38115a055a1d3487d07da187b0988c219cf030cc
woojin <ooojin523@gmail.com>
2026-04-04 17:32:54 +0900

    feat: scheduleStatus Enum 타입 수정

--- src/main/java/com/mmgon/jjajuka/global/enums/ScheduleStatus.java
+++ src/main/java/com/mmgon/jjajuka/global/enums/ScheduleStatus.java
@@ -1,6 +1,10 @@
 package com.mmgon.jjajuka.global.enums;
 
 public enum ScheduleStatus {
-    PENDING, COMPLETED, CANCLED
+    ACTIVE,             // 현재 유효한 배정
+    SWAP_PENDING,       // 교대 요청 진행 중
+    VACANCY_PENDING,    // 결원 요청 진행 중
+    CANCELED,           // 취소된 배정
+    SWAPPED             // 교대로 인해 원래 배정이 변경됨
 }
 
